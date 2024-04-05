f0a018cd22c83a3f919e3010e6b766921b9b86cc
suzan-sheikh <mdsuzanskh@gmail.com>
2024-04-04 23:10:10 -0700

    update again

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -2,7 +2,6 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "../layouts/Root";
 import Home from "../pages/Home/Home";
 import Login from "../pages/Login/Login";
-
 const router = createBrowserRouter([
     {
         path: '/',
